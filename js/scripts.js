7102673ff6c106b9d7f8a1a7f7a47dccde4099d9
Vanessa <vanessaangel.ux@gmail.com>
2022-06-11 14:58:11 +0200

    Js menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log ("Hello World")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle' && $(window).width() < 640) {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
